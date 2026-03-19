bb9ec8e62d99c8c240d33fa39048a06b1113917f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 21:08:51 +0530

    feat: add formatDate function to format article dates

--- app/ui/articles.tsx
+++ app/ui/articles.tsx
@@ -5,4 +5,13 @@ import Image from "next/image";
 
 export default function Articles({ articles }: { articles: Promise<any> }) {
   const allArticles = use(articles);
+  const formatDate = (date: Date) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+  };
+
 }
