4607f957d550a59d03cdb7b372e023975fe7343b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 21:09:19 +0530

    feat: enhance Articles component to display articles with formatting

--- app/ui/articles.tsx
+++ app/ui/articles.tsx
@@ -13,5 +13,35 @@ export default function Articles({ articles }: { articles: Promise<any> }) {
     };
     return new Date(date).toLocaleDateString("en-US", options);
   };
-
+  return (
+    <ul>
+      <div>
+        <h2 className="text-2xl font-semibold mb-6">Articles</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {allArticles.data.map((article) => (
+            <article
+              key={article.id}
+              className="bg-white shadow-md rounded-lg overflow-hidden"
+            >
+              <Image
+                className="w-full h-48 object-cover"
+                src={process.env.NEXT_PUBLIC_STRAPI_URL + article.cover.url}
+                alt={article.title}
+                width={180}
+                height={38}
+                priority
+              />
+              <div className="p-4">
+                <h3 className="text-lg font-bold mb-2">{article.title}</h3>
+                <p className="text-gray-600 mb-4">{article.content}</p>
+                <p className="text-sm text-gray-500">
+                  Published: {formatDate(article.publishedAt)}
+                </p>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </ul>
+  );
 }
