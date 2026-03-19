828e8362441638fc8c800368703374b12ab3da03
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 21:48:52 +0530

    feat: add getArticles function to fetch articles in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import { Suspense } from "react";
 import Articles from "./ui/articles";
 
+const getArticles = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/articles?populate=*`,
+  );
+  const articles = await response.json();
+  return articles;
+};
+
 const page = () => {
-  
+  const articles = getArticles();
   return (
     <div className="p-6">
       <h1 className="text-4xl font-bold mb-8">
