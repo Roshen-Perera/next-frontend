ace457ce397d7c6e39a3570b239608b427b03b34
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 21:10:25 +0530

    feat: add getArticles function to fetch articles from API

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import { Suspense } from "react";
 import Articles from "./ui/articles";
 
+const getArticles = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/articles?populate=*`,
+  );
+  const articles = await response.json();
+  return articles;
+};
+
 const page = () => {
   return <div></div>;
 };
