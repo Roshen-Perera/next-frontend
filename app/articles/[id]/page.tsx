4197a3e56a80d69552a8e1eadb011dff7c1e1f45
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-10 18:01:48 +0530

    feat: implement ArticleDetailPage component with dynamic article ID display

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,12 +1,16 @@
-
 interface Props {
   params: {
     id: string;
   };
 }
 
-interface Props {
-  params: {
-    id: string;
-  };
-}
+const ArticleDetailPage = ({ params }: Props) => {
+  return (
+    <div>
+      <h1>Article {params.id}</h1>
+      {/* Article detail content goes here */}
+    </div>
+  );
+};
+
+export default ArticleDetailPage;
